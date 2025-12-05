cdb554f486dd9e8aed19b7224ccc5e21afd01283
sazzy789 <satyamwork789@gmail.com>
2025-12-05 13:34:45 +0530

    first express code

---
+++ index.js
@@ -0,0 +1,23 @@
+//creating an HTTP server
+//express
+//express is not a node default library
+
+const express = require('express');
+
+const app = express();
+
+//calculate sum till number
+function calculate(n) {
+    let ans = 0;
+    for (let i = 0; i <= n; i++) {
+        ans = ans + i;
+    }
+    return ans;
+}
+
+app.get("/", function (req, res) {
+    const  n = req.query.n;
+    const ans = calculate(n);
+    res.send("hi there your sum is: " + ans.toString() );
+})
+app.listen(3000);
