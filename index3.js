7e3dd45bd8526e1a1f20261e4020ee749f250458
sazzy789 <satyamwork789@gmail.com>
2025-12-06 01:56:34 +0530

    calling middlewar centrally

--- index3.js
+++ index3.js
@@ -13,14 +13,19 @@ function checkAgeMiddleware(req, res, next) {
     }
 }
 
+//since we are calling the middleware in each routes, we can call it above these routes, at one place , like this below, will reduce funtion signature
+app.use(checkAgeMiddleware);
+
+//NOTE: all routes above this , will not be able to access middlewares
+
 //calling middleware : calls the next middleware fns in the stack
-app.get("/ride1",checkAgeMiddleware, function (req, res) {
+app.get("/ride1", function (req, res) {
     res.status(200).json({
         msg: "you have successfully riden ride1"
     })
 })
 
-app.get("/ride2",checkAgeMiddleware, function (req, res) {
+app.get("/ride2", function (req, res) {
     res.status(200).json({
         msg: "you have successfully riden ride2"
     })
