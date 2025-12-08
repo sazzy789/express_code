b75b756a188d40ac1b6661b7ea052a32ea97e925
sazzy789 <satyamwork789@gmail.com>
2025-12-09 00:35:06 +0530

    middleware question

--- operations.js
+++ operations.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
+//Create a middleware function that logs each incoming request’s HTTP method, URL, and timestamp to the console
+app.use(function (req, res, next) {
+    const timestamp = new Date();
+    console.log('[' + timestamp + ']' + req.method + '  ' + req.originalUrl);
+    next();
+})
+
 app.get('/multiply', function (req, res) {
     const n1 = req.query.n1;
     const n2 = req.query.n2;
-
     var product = n1 * n2;
     res.status(200).json({
         "product": product
     });
 })
-//using URL params
-// Use URL params for resources.
-// Use query params for operations.
+
 app.get('/add/:firstArg/:secondArg', function (req, res) {
     const n1 = parseInt(req.params.firstArg);
     const n2 = parseInt(req.params.secondArg);
