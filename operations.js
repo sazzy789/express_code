b292b342769e9b36bbd1ef1afba40421cbca96e1
sazzy789 <satyamwork789@gmail.com>
2025-12-09 00:06:18 +0530

    syntax for URL params

--- operations.js
+++ operations.js
@@ -10,11 +10,12 @@ app.get('/multiply', function (req, res) {
         "product": product
     });
 })
-//we used parseInt to convert into Integer
-//check operation_fixed.png
-app.get('/add', function (req, res) {
-    const n1 = parseInt(req.query.n1);
-    const n2 = parseInt(req.query.n2);
+//using URL params
+// Use URL params for resources.
+// Use query params for operations.
+app.get('/add/:firstArg/:secondArg', function (req, res) {
+    const n1 = parseInt(req.params.firstArg);
+    const n2 = parseInt(req.params.secondArg);
 
     var sum = n1 + n2;
     res.status(200).json({
